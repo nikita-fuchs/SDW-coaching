d18bd150eebe8dba14f4fce75ed225ae6ef547bb
bella754 <isabella.benser@yahoo.de>
2020-03-14 13:54:09 +0100

    @sinnfrei

---
+++ random.js
@@ -0,0 +1,19 @@
+console.log ("hallo");
+
+let goodWeather = true;
+
+if (goodWeather){
+    console.log("go outside");
+}else{
+    console.log("stay inside");
+}
+
+let tiredLevel = 6;
+
+if (tiredLevel >=5) {
+    console.log("bedtime!");
+}else{
+    console.log("do something useful!");
+}
+ 
+
