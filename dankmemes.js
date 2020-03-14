a4a84713e63279a2f21e14a623d6b4fe70d8951f
wastedsoviet <61596335+wastedsoviet@users.noreply.github.com>
2020-03-14 14:12:27 +0100

    Update dankmemes.js

--- dankmemes.js
+++ dankmemes.js
@@ -3,7 +3,7 @@ console.log("macht die Musik leiser");
 jonas = true;
 
 if (jonas) {
-  console.log("JONAS UND SEINE MUTTER MACHEN VERWANDTSCHAFT+");
+  console.log("YANNICK UND SEINE FAM MACHEN VERWANDTSCHAFT+");
 }
 
 console.log("hello world");
