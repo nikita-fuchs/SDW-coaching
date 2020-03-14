eb7ac960facf2aba4d80a0df54ebed696d4e730f
Jonas Geiseler <jonasgeiseler@Jonas-MacBook-Air.local>
2020-03-14 14:03:15 +0100

    Yannickistgay

---
+++ yannickderhuso.js
@@ -0,0 +1,21 @@
+//console.log('Fabian ist ein Krüppel.');
+let FabiCripple = true;
+if (FabiCripple) {
+    console.log('Jonas hit Fabians ankel, thats very effective!')
+} else {
+    console.log('Fabian escaped!')
+}
+
+var benzer = false;
+
+if (benzer) {
+    console.log('Und noch ein Benzer!')
+} else {
+    console.log('*sad benzer noise*')
+}
+
+var yannick = true;
+
+if (yannick) {
+    console.log('Yannick aka der Hurensohn')
+}
